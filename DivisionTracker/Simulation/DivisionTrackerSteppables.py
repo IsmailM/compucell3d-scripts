7ed5a05a362565941057a4be2f4ee4b95ea6af19
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-05-20 15:18:11 -0400

    Add home made division tracker steppables

---
+++ DivisionTracker/Simulation/DivisionTrackerSteppables.py
@@ -0,0 +1,87 @@
+
+from PySteppables import *
+import CompuCell
+import sys
+
+from PySteppablesExamples import MitosisSteppableBase
+            
+
+class ConstraintInitializerSteppable(SteppableBasePy):
+    def __init__(self,_simulator,_frequency=1):
+        SteppableBasePy.__init__(self,_simulator,_frequency)
+    def start(self):
+        for cell in self.cellList:
+            cell.targetVolume=25
+            cell.lambdaVolume=2.0
+
+        
+        
+
+class GrowthSteppable(SteppableBasePy):
+    def __init__(self,_simulator,_frequency=1):
+        SteppableBasePy.__init__(self,_simulator,_frequency)
+    def step(self,mcs):
+        for cell in self.cellList:
+            cell.targetVolume+=0.01 if cell.targetVolume != 0 else 0    
+            if cell.targetVolume == 0 :
+                print 'not growing',cell.targetVolume
+            else:
+                print 'growing',cell.targetVolume  
+
+    # alternatively if you want to make growth a function of chemical concentration uncomment lines below and comment lines above        
+        # field=CompuCell.getConcentrationField(self.simulator,"PUT_NAME_OF_CHEMICAL_FIELD_HERE")
+        # pt=CompuCell.Point3D()
+        # for cell in self.cellList:
+            # pt.x=int(cell.xCOM)
+            # pt.y=int(cell.yCOM)
+            # pt.z=int(cell.zCOM)
+            # concentrationAtCOM=field.get(pt)
+            # cell.targetVolume+=0.01*concentrationAtCOM  # you can use here any fcn of concentrationAtCOM     
+        
+        
+
+class MitosisSteppable(MitosisSteppableBase):
+    def __init__(self,_simulator,_frequency=1):
+        MitosisSteppableBase.__init__(self,_simulator, _frequency)
+    
+    def step(self,mcs):
+        # print "INSIDE MITOSIS STEPPABLE"
+        cells_to_divide=[]
+        for cell in self.cellList:
+            if cell.volume>28:
+                print 'dividing now'
+                cells_to_divide.append(cell)
+                
+        for cell in cells_to_divide:
+            # to change mitosis mode leave one of the below lines uncommented
+            self.divideCellRandomOrientation(cell)
+            # self.divideCellOrientationVectorBased(cell,1,0,0)                 # this is a valid option
+            # self.divideCellAlongMajorAxis(cell)                               # this is a valid option
+            # self.divideCellAlongMinorAxis(cell)                               # this is a valid option
+
+    def updateAttributes(self):
+        parentCell=self.mitosisSteppable.parentCell
+        childCell=self.mitosisSteppable.childCell
+        print 'parent:',parentCell.id
+        print 'child:',childCell.id
+        childCell.targetVolume=parentCell.targetVolume
+        childCell.lambdaVolume=parentCell.lambdaVolume
+        if parentCell.type==self.TUMOR:
+            childCell.type=self.TUMOR
+        else:
+            childCell.type=self.TUMOR
+        
+        
+
+class DeathSteppable(SteppableBasePy):
+    def __init__(self,_simulator,_frequency=1):
+        SteppableBasePy.__init__(self,_simulator,_frequency)
+    def step(self,mcs):
+        if mcs==300:
+            for cell in self.cellList:
+                if cell.type==self.TUMOR:
+                    cell.targetVolume=0
+                    cell.lambdaVolume=100
+                    print 'dying now\n\n\n\n\n\n\n\n'
+        
+        
